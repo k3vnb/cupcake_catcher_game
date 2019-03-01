5eaf8ad370fc7b62a84597e5693131bd1f356f56
Kevin Boyle <papershack@gmail.com>
2019-02-28 17:53:21 -0800

    set up game, draw initial tiles to canvas

--- index.js
+++ index.js
@@ -9,6 +9,19 @@ const tile = new Image();
 const fruit = new Image();
 const food = new Image();
 
+//Global variables
+let score = 0;
+let level = 100;
+let animation = 0;
+let foodTimer = 0;
+let gameover = false;
+let intervalvar;
+let foodList = [];
+let tileList = [];
+    //food will drop from the foodDrop positions 
+const foodDrop = [0,50,100,150,200,250,300,350,400,450];
+
+
 const tileObject = {
     'width': 50,
     'height': 20
@@ -35,17 +48,17 @@ const catcher = {
     'safe': true
 }
 
-background.onload = function(){
-    blood.onload = function(){
-        catcherOne.onload = function(){
-            catcherTwo.onload = function(){
-                catcherThree.onload = function(){
-                    catcherFour.onload = function(){
-                        food.onload = function(){
-                            fruit.onload = function(){
-                                tile.onload = function(){
-                                    console.log("callback HELL achieved!");
-
+//wait till assets load, then call startGame()
+background.onload = () => {
+    blood.onload = () => {
+        catcherOne.onload = () => {
+            catcherTwo.onload = () => {
+                catcherThree.onload = () => {
+                    catcherFour.onload = () => {
+                        food.onload = () => {
+                            fruit.onload = () => {
+                                tile.onload = () => {
+                                    startGame()
                                 }
                                 tile.src = "images/tile.png"
                             }
@@ -64,3 +77,32 @@ background.onload = function(){
     blood.src = "images/blood.png"
 }
 background.src = "images/background.jpg";
+
+
+drawObject = (object, x, y, width, height) => {
+    ctx.drawImage(object,x,y,width,height);
+}
+
+startGame = () => {
+    score = 0;
+    level = 100;
+    catcher.x = 100;
+    catcher.y = 350;
+    catcher.onair = false;
+    catcher.leftPressed = false;
+    catcher.rightPressed = false;
+    catcher.safe = true;
+    animation = 0;
+    foodTimer = 0;
+    gameover = false;
+    tileList = [];
+    foodList = [];
+
+    for (let i=0; i<=9; i++){
+        tileList.push({'x':i*50, 'y':400})
+    }
+
+    for (let i=0; i<tileList.length; i++){
+        drawObject(tile, tileList[i].x, tileList[i].y, tileObject.width, tileObject.height);
+    }
+}
